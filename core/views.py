02a3aa30f89d24a99e881427d53994d35db25059
stephenmcd <steve@jupo.org>
2011-07-30 18:28:25 +1000

    Redis integration and join/leave events

--- core/views.py
+++ core/views.py
@@ -1,4 +1,6 @@
 
+import redis
+
 from django.contrib.auth.decorators import login_required
 from django.contrib.auth import logout as auth_logout
 from django.contrib import messages
@@ -9,19 +11,49 @@ from core.forms import DrawingForm
 from core.models import Drawing
 
 
+pool = redis.ConnectionPool()
+
 def socketio(request):
+    """
+    Socket.IO handler.
+    """
     socket = request.environ["socketio"]
-    while True:
-        message = socket.recv()
-        if len(message) > 0:
-            socket.broadcast(message)
-        else:
-            if not socket.connected():
-                break
+    r = redis.Redis(connection_pool=pool)
+    try:
+        while True:
+            message = socket.recv()
+            if len(message) > 0:
+                drawing_id, action = message[:2]
+                drawing_key = "drawing-%s" % drawing_id
+                user_key = "users-%s" % drawing_id
+                if action == "join":
+                    # Add the user to the user set.
+                    r.sadd(user_key, ",".join(message[2:]))
+                    # Get all users with args prepended to look like join actions.
+                    user_actions = [s for m in r.smembers(user_key) for s in [drawing_id, "join"] + m.split(",")]
+                    # Get all the draw actions.
+                    drawing_actions = [s for m in r.lrange(drawing_key, 0, -1) for s in m.split(",")]
+                    # Dump the combined action list to the joining user.
+                    socket.send(drawing_actions + user_actions)
+                elif action == "leave":
+                    # Remove the user from the user set.
+                    r.srem(user_key, ",".join(message[2:]))
+                else:
+                    # Add the draw action.
+                    r.rpush(drawing_key, ",".join(message))
+                socket.broadcast(message)
+            else:
+                if not socket.connected():
+                    break
+    except Exception, e:
+        print e
     return HttpResponse("")
 
 @login_required
 def drawing_new(request, template="new.html"):
+    """
+    Creates a new drawing.
+    """
     form = DrawingForm(request.POST or None)
     if request.method == "POST":
         if form.is_valid():
@@ -35,20 +67,32 @@ def drawing_new(request, template="new.html"):
 
 @login_required
 def drawing_view(request, slug, template="view.html"):
+    """
+    Display a drawing.
+    """
     drawing = get_object_or_404(Drawing, slug=slug)
     context = {"drawing": drawing}
     return render(request, template, context)
 
 def drawing_list(request, template="list.html"):
+    """
+    List all drawings.
+    """
     context = {"drawings": Drawing.objects.all()}
     return render(request, template, context)
 
 def loggedin(request):
+    """
+    social-auth callback redirected to when user logs in.
+    """
     if request.user.is_authenticated():
         messages.success(request, "Logged in")
     return redirect(request.COOKIES.get("next", "home"))
 
 def logout(request):
+    """
+    Log out.
+    """
     if request.user.is_authenticated():
         auth_logout(request)
     messages.success(request, "Logged out")
--- static/js/drawnby.js
+++ static/js/drawnby.js
@@ -9,20 +9,36 @@ $(function() {
     drawing = false;
     actions = {};
 
-    actions.mousedown = function(x, y) {
+    actions.mousedown = function(x, y, username, userID) {
         context.beginPath();
         context.moveTo(x, y);
     };
 
-    actions.mousemove = function(x, y) {
+    actions.mousemove = function(x, y, username, userID) {
         context.lineTo(x, y);
         context.stroke();
     };
 
+    actions.join = function(username, userID) {
+        if ($('#user-' + userID).length == 0) {
+            $('#users').append('<li id="user-' + userID + '">' + username + '</li>');
+        }
+    };
+
+    actions.leave = function(username, userID) {
+        if (userID != window.userID) {
+            $('#user-' + userID).remove();
+        }
+    };
+
     var action = function(args) {
-        if (args.shift() == window.drawingID) {
-            var action = actions[args.shift()];
-            action.apply(null, args);
+        while (args.length > 0) {
+            if (args.shift() == window.drawingID) {
+                var action = actions[args.shift()];
+                var argLength = action.prototype.constructor.length;
+                action.apply(null, args.slice(0, argLength));
+                args = args.slice(argLength);
+            }
         }
     };
 
@@ -33,7 +49,14 @@ $(function() {
 
     var socket = new io.Socket();
     socket.connect();
+    socket.on('connect', function() {
+        send('join');
+    });
+    $(window).unload(function() {
+        send('leave');
+    });
     socket.on('message', function(args) {
+        console.log(args)
         action(args);
     });
 
@@ -41,6 +64,7 @@ $(function() {
         var getArgs = function(args) {
             args = $.makeArray(args);
             args.unshift(window.drawingID);
+            args.push(window.username, window.userID);
             return args;
         };
         action(getArgs(arguments));
