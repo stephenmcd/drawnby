c97aec7460ad5a8e43a324987b790d2e4b2dda0b
stephenmcd <steve@jupo.org>
2011-07-30 12:57:41 +1000

    Add admin

--- urls.py
+++ urls.py
@@ -1,9 +1,13 @@
 
 from django.conf.urls.defaults import *
 from django.conf import settings
+from django.contrib import admin
 
+
+admin.autodiscover()
 
 urlpatterns = patterns("",
+    ("^admin/", include(admin.site.urls)),
     url("", include("core.urls")),
     url("", include("social_auth.urls")),
     ("^%s/(?P<path>.*)$" % settings.MEDIA_URL.strip("/"),
