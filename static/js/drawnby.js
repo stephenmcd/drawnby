e5bf8c483f22b156922f630b400bf2eca78b1a86
stephenmcd <steve@jupo.org>
2011-07-30 21:11:37 +1000

    Clean up and comment js

--- static/js/drawnby.js
+++ static/js/drawnby.js
@@ -1,38 +1,60 @@
 $(function() {
 
+    // Set up the canvas context.
     var canvas = $('#canvas');
     var context = document.getElementById('canvas').getContext('2d');
     context.width = canvas.width();
     context.height = canvas.height();
     context.lineCap = 'round';
     context.lineJoin = 'round';
+
+    // Mousedown/drawing flag.
     drawing = false;
-    actions = {};
 
-    actions.mousedown = function(x, y, username, userID) {
-        context.beginPath();
-        context.moveTo(x, y);
-    };
+    // Container that stores each of the actions so that they can
+    // be referenced dynamically by name. Triggered client-side by
+    // the current user, or from the server via Socket.IO for all
+    // other users for this drawing.
+    var actions = {
 
-    actions.mousemove = function(x, y, username, userID) {
-        context.lineTo(x, y);
-        context.stroke();
-    };
+        // Start drawing.
+        mousedown: function(x, y, username, userID) {
+            context.beginPath();
+            context.moveTo(x, y);
+        },
 
-    actions.join = function(username, userID) {
-        if ($('#user-' + userID).length == 0) {
-            $('#users').append('<li id="user-' + userID + '">' +
-                               '<img src="' + window.MEDIA_URL + 'photos/' + userID + '.20x20_q85_crop-smart.jpg">' +
-                               username + '</li>');
+        // Draw.
+        mousemove: function(x, y, username, userID) {
+            context.lineTo(x, y);
+            context.stroke();
+        },
+
+        // User joining - add their name to the user list.
+        join: function(username, userID) {
+            if ($('#user-' + userID).length == 0) {
+                $('#users').append('<li id="user-' + userID + '">' +
+                                   '<img src="' + window.MEDIA_URL + 'photos/' + userID + '.20x20_q85_crop-smart.jpg">' +
+                                   username + '</li>');
+            }
+        },
+
+        // User leaving - remove their name from the user list.
+        leave: function(username, userID) {
+            if (userID != window.userID) {
+                $('#user-' + userID).remove();
+            }
         }
+
     };
 
-    actions.leave = function(username, userID) {
-        if (userID != window.userID) {
-            $('#user-' + userID).remove();
-        }
-    };
-
+    // Takes an array of args that represent one or more actions to call.
+    // First arg is always the drawing ID as a guard to ensure actions are
+    // only performed for the current drawing. Second arg is the act‭ion name
+    // and the rest are args for that action. Since many sets of actions
+    // can be sent from the server in one batch, we keep looping and test
+    // for the argument length of the action being called, pulling the
+    // required number of arguments off the list, until the entire list
+    // is empty.
     var action = function(args) {
         while (args.length > 0) {
             if (args.shift() == window.drawingID) {
@@ -44,24 +66,11 @@ $(function() {
         }
     };
 
-    var getCoords = function(event) {
-        var offset = canvas.offset();
-        return {x: event.pageX - offset.left, y: event.pageY - offset.top};
-    };
-
-    var socket = new io.Socket();
-    socket.connect();
-    socket.on('connect', function() {
-        send('join');
-    });
-    $(window).unload(function() {
-        send('leave');
-    });
-    socket.on('message', function(args) {
-        console.log(args)
-        action(args);
-    });
-
+    // The main handler for actions triggered client-side.
+    // First wrap the arguments with the drawing ID and user vars,
+    // perform the actual action client-side, and pass the action
+    // and arguments off to the server via socket.io for broadcasting
+    // to other users.
     var send = function() {
         var getArgs = function(args) {
             args = $.makeArray(args);
@@ -73,16 +82,38 @@ $(function() {
         socket.send(getArgs(arguments));
     };
 
+    // Socket.IO setup.
+    var socket = new io.Socket();
+    socket.connect();
+    socket.on('connect', function() {
+        send('join');
+    });
+    $(window).unload(function() {
+        send('leave');
+    });
+    socket.on('message', function(args) {
+        action(args);
+    });
+
+    // Cross-browser pixel offset.
+    var getCoords = function(event) {
+        var offset = canvas.offset();
+        return {x: event.pageX - offset.left, y: event.pageY - offset.top};
+    };
+
+    // Stop drawing on mouseup.
     canvas.mouseup(function() {
         drawing = false;
     });
 
+    // Start drawing on mousedown.
     canvas.mousedown(function(event) {
         var coords = getCoords(event);
         drawing = true;
         send('mousedown', coords.x, coords.y);
     });
 
+    // Draw on mousemove if drawing is currently on (eg mouse is down).
     canvas.mousemove(function(event) {
         if (drawing) {
             var coords = getCoords(event);
