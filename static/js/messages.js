a80e7f0f378d01b016a920dd083883c32fe9aee4
stephenmcd <steve@jupo.org>
2011-07-31 20:54:49 +1000

    Add styling and effects for messages

---
+++ static/js/messages.js
@@ -0,0 +1,27 @@
+
+var messages = {
+
+    add: function(msg) {
+        $('#message-template').tmpl({msg: msg}).appendTo('#messages');
+        messages.show();
+    },
+
+    show: function() {
+        $('.message').slideDown();
+        clearTimeout(messages.timeout);
+        messages.timeout = setTimeout(function() {
+            $('.message').fadeOut();
+        }, 5000);
+    },
+
+    timeout: null
+
+};
+
+$(function() {
+    $('.message a').live('click', function() {
+        $('.message').fadeOut();
+        return false;
+    });
+    messages.show();
+});
