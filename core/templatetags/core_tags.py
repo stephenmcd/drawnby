afaa8947b2112cf60baf30edcd7fd9ba53fc6f99
stephenmcd <steve@jupo.org>
2011-07-31 14:23:19 +1000

    Update in progress list to query redis.

--- core/templatetags/core_tags.py
+++ core/templatetags/core_tags.py
@@ -2,15 +2,30 @@
 from django.template import Library
 
 from core.models import Drawing
+from core.utils import redis
 
 
 register = Library()
 
 @register.filter
 def photo_for_user(user):
+    """
+    Just returns the path to the user's photo since we can't
+    combined values in a template to pass to the thumbnail tag.
+    """
     return "photos/%s" % user.id
 
 @register.simple_tag(takes_context=True)
 def load_in_progress(context):
-    context["progress"] = Drawing.objects.filter(data__isnull=True)
+    """
+    Returns the keys and numbers of users for each of the drawings
+    in progress.
+    """
+    progress = []
+    for key in redis.keys("users-*"):
+        progress.append({
+            "key": key.split("-")[1],
+            "users": [u.split(",")[0] for u in redis.smembers(key)],
+        })
+    context["progress"] = sorted(progress, key=lambda x: len(x["users"]), reverse=True)
     return ""
