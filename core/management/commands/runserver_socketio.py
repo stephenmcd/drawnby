29c09407af3bb6941579347adb74cd7ba0eb0eb0
stephenmcd <steve@jupo.org>
2011-07-31 11:26:58 +1000

    Add a reloader for socketio command

--- core/management/commands/runserver_socketio.py
+++ core/management/commands/runserver_socketio.py
@@ -1,11 +1,26 @@
 
 from re import match
+from thread import start_new_thread
+from time import sleep
+from os import getpid, kill
+from signal import SIGINT
 
 from django.core.handlers.wsgi import WSGIHandler
-from django.core.management.base import BaseCommand
+from django.core.management.base import BaseCommand, CommandError
 from django.core.management.commands.runserver import naiveip_re
+from django.utils.autoreload import code_changed, restart_with_reloader
 from socketio import SocketIOServer
 
+
+RELOAD = False
+
+def reload_watcher():
+    global RELOAD
+    while True:
+        RELOAD = code_changed()
+        if RELOAD:
+            kill(getpid(), SIGINT)
+        sleep(1)
 
 class Command(BaseCommand):
 
@@ -21,9 +36,18 @@ class Command(BaseCommand):
                                    'or address:port pair.' % addrport)
             self.addr, _, _, _, self.port = m.groups()
 
-        bind = (self.addr, int(self.port))
-        print
-        print "SocketIOServer running on %s:%s" % bind
-        print
-        server = SocketIOServer(bind, WSGIHandler(), resource="socket.io")
-        server.serve_forever()
+        start_new_thread(reload_watcher, ())
+        try:
+            bind = (self.addr, int(self.port))
+            print
+            print "SocketIOServer running on %s:%s" % bind
+            print
+            server = SocketIOServer(bind, WSGIHandler(), resource="socket.io")
+            server.serve_forever()
+        except KeyboardInterrupt:
+            if RELOAD:
+                print
+                print "Reloading..."
+                restart_with_reloader()
+            else:
+                raise
